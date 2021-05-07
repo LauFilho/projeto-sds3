ae982ab7ba4a6a6decb19a73b8f4f0da46e570e9
Laudelino Filho <laualmeidafilho@hotmail.com>
2021-05-06 23:28:06 -0300

    Adicionado os gráficos estáticos com dados "Mockados"

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,4 +1,6 @@
+import BarChart from "components/barchart";
 import DataTable from "components/dataTable";
+import DonutChart from "components/donutchart/indext";
 import Footer from "components/footer";
 import NavBar from "components/navbar";
 
@@ -7,9 +9,22 @@ function App() {
   return (
     <>
       <NavBar />
-      <div className = "container">
-       <h1 className="text-primary" >Hello Brothers</h1>
-      <DataTable/>
+      <div className="container">
+        <h1 className="text-primary py-3" >Dashboard de Vendas</h1>
+        <div className="row px-3" >
+          <div className="col-sm-6">
+            <h5 className="text-center text-secondary"> Taxa de Sucesso (%)</h5>
+            <BarChart />
+          </div>
+          <div className="col-sm-6">
+          <h5 className="text-center text-secondary"> Taxa de Sucesso</h5>
+            <DonutChart/>
+          </div>
+          <h2 className="text-primary py-4" >Todas as Vendas</h2>
+          <DataTable />
+          
+
+        </div>
       </div>
       <Footer></Footer>
     </>
